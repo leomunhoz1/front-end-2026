c1622f405239b1875296d0ff9773089501aa51ea
leomunhoz1 <leofranciscomunhoz@gmail.com>
2026-05-14 20:12:35 -0300

    Add files via upload

---
+++ PROVA 2/script.js
@@ -0,0 +1,5 @@
+    const img = document.getElementById("ovo");
+
+    function add() {
+        img.style.display = "block";
+    }
---
+++ PROVA 3/script.js
@@ -0,0 +1,5 @@
+const img = document.getElementById("sss");
+
+function revelar() {
+    img.style.display = "block";
+}
